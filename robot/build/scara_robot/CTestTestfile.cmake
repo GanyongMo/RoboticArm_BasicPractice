e4cf2d559e0a0944ad0fe3997d11578bc5029096
Momo <ganyong.mo@e-campus.uab.cat>
2022-04-15 11:28:09 +0200

    URDF_SCARA

---
+++ robot/build/scara_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/momo/robot/src/scara_robot
+# Build directory: /home/momo/robot/build/scara_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
